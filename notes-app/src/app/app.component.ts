58cceac13029e050a6a4c6f11d5828d1be49cef0
akshay <akshay.kumar@keyvertex.com>
2020-03-22 19:05:32 +0530

    data transmission

--- notes-app/src/app/app.component.ts
+++ notes-app/src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'notes-app';
+  selectedNoteItem; textForSearch;
+  isDisabled;
+
+  selectedNote(e) {
+    this.selectedNoteItem = e;
+    console.log("AppComponent -> selectedNote -> this.selectedNoteItem", this.selectedNoteItem);
+  }
+
+  searchedText(e) {
+    this.textForSearch = e;
+  }
+
+  isDisable(e) {
+    this.isDisabled = e;
+    console.log("AppComponent -> isDisable -> this.isDisabled", this.isDisabled);
+  }
 }
