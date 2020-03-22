4f0f3648b36613b7eb4f7a54a348859043d472ae
akshay <akshay.kumar@keyvertex.com>
2020-03-22 15:59:58 +0530

    collapsable side bar

--- notes-app/src/app/navbar/navbar.component.ts
+++ notes-app/src/app/navbar/navbar.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  icon:string = 'expand-1';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    this.icon = 'collapse';
+  }
+
+  closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    this.icon = 'expand-1';
   }
 
 }
