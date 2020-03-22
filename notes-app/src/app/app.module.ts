303e272e9cc4b0298e16e2b3f0a7b3c68149b9de
akshay <akshay.kumar@keyvertex.com>
2020-03-22 16:02:19 +0530

    images and grid in layout

--- notes-app/src/app/app.module.ts
+++ notes-app/src/app/app.module.ts
@@ -4,16 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { WriteNotesComponent } from './write-notes/write-notes.component';
+import { FormsModule } from '@angular/forms';
+import { SideBarComponent } from './side-bar/side-bar.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    WriteNotesComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
